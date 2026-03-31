ea32d1c9caceb263729c6a9c648925f28a0a5d02
GoldForContinent <manukip642@gmail.com>
2026-03-31 22:41:46 +0300

    enlarge logo

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -87,7 +87,7 @@ export default function Layout({ children }: LayoutProps) {
       >
         <div className="px-4 md:px-[4vw] flex items-center justify-between">
           <Link to="/" className="hover:opacity-80 transition-opacity">
-            <img src="/logo3.png" alt="Rays of Africa Logo" className="h-12 w-auto" />
+            <img src="/logo3.png" alt="Rays of Africa Logo" className="h-20 w-auto" />
           </Link>
           
           {/* Desktop Nav */}
