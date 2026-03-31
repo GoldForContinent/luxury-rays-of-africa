8110c477d74a60323204aae0c56c66f1c1b951d0
GoldForContinent <manukip642@gmail.com>
2026-03-31 22:27:23 +0300

    logo add

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -86,12 +86,8 @@ export default function Layout({ children }: LayoutProps) {
         className="fixed top-0 left-0 right-0 z-50 py-4 bg-transparent"
       >
         <div className="px-4 md:px-[4vw] flex items-center justify-between">
-          <Link to="/" className="inline-block">
-            <img 
-              src="/dist/logo2.png" 
-              alt="Rays of Africa"
-              className="h-12 w-auto hover:opacity-80 transition-opacity"
-            />
+          <Link to="/" className="text-2xl font-bold text-[#D4A03A] hover:opacity-80 transition-opacity">
+            Rays of Africa
           </Link>
           
           {/* Desktop Nav */}
