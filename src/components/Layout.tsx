b06ddc64aba62e44f1f034d919fd1ce8d2240347
GoldForContinent <manukip642@gmail.com>
2026-03-31 23:03:49 +0300

    navfinale

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -82,7 +82,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen bg-black">
       {/* Navigation */}
-      <nav className="fixed top-0 left-0 right-0 z-50 py-4">
+      <nav className="fixed top-0 left-0 right-0 z-50 py-4" style={{ background: 'transparent' }}>
         <div className="px-4 md:px-[4vw] flex items-center justify-between gap-8">
           {/* Logo - Left */}
           <Link to="/" className="hover:opacity-80 transition-opacity shrink-0">
