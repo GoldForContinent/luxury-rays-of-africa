a807c9f8e8ec74be05b99427e49325add0b0198b
GoldForContinent <manukip642@gmail.com>
2026-04-01 22:38:23 +0300

    logo update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -86,7 +86,7 @@ export default function Layout({ children }: LayoutProps) {
         <div className="px-4 md:px-[4vw] flex items-center justify-between gap-8">
           {/* Logo - Left */}
           <Link to="/" className="hover:opacity-80 transition-opacity shrink-0">
-            <img src="/logo3.png" alt="Rays of Africa Logo" className="h-24 w-auto" />
+            <img src="/logo4.png" alt="Rays of Africa Logo" className="h-24 w-auto" />
           </Link>
           
           {/* Desktop Nav - Center */}
